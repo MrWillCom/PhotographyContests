d99a2fc4c9ac6e0c3fa2ca6de0a580307b87d958
Mr. Will <mr.will.com@outlook.com>
2024-01-27 17:23:26 +0800

    Fix `toSorted` is not a function

--- src/data/2023/index.ts
+++ src/data/2023/index.ts
@@ -1,7 +1,9 @@
 import { promises as fs } from 'fs'
 
 export default async function getContests() {
-  return (await fs.readdir(process.cwd() + '/src/data/2023', 'utf8'))
+  const files = await fs.readdir(process.cwd() + '/src/data/2023', 'utf8')
+
+  return files
     .map(file => {
       if (file.endsWith('.json')) {
         return require(`./${file}`)
